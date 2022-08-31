9ba476d105f7ce0c1c7b57e0ddb7373275942cb3
swaraj203@gmail.com <swaraj203@gmail.com>
2022-08-31 14:38:23 +0530

    First Commit

---
+++ src/main/java/MyHome.java
@@ -0,0 +1,8 @@
+
+
+
+public class MyHome {
+    public static void main(String[] args){
+        System.out.println("Hello World");
+    }
+}
---
+++ src/test/java/SampleTest/JenkinsTest.java
@@ -0,0 +1,15 @@
+package SampleTest;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class JenkinsTest {
+
+    @Test
+    public void helloWorldTest(){
+        int a=10;
+        int b=20;
+        System.out.println("a="+a+" b="+b);
+        Assert.assertTrue(a!=b);
+    }
+}
